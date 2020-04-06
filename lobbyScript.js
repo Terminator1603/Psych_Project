4fd0bdba20a5f60b626450441885d520628b5ca3
rohit londhe <lrohitv@gmail.com>
2020-04-07 00:34:31 +0530

    halp

---
+++ lobbyScript.js
@@ -0,0 +1,8 @@
+
+var mydiv;
+var odiv = document.getElementsByClassName('main')[0];
+
+  mydiv= document.createElement('DIV');
+  mydiv.className=('lobby_user');
+  mydiv.innerHTML = "hello";
+  odiv.appendChild(mydiv);
