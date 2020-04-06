eba47a5b07eb6db31a90eeceb050933966479594
Yash Burshe <bursheyash@icloud.com>
2020-04-07 01:23:47 +0530

    prettier

--- lobbyScript.js
+++ lobbyScript.js
@@ -1,14 +1,11 @@
-var names = ['yash','rohit','akis'];
-var i=0;
+var names = ["yash", "rohit", "akis"];
+var i = 0;
 var mydiv;
 var odiv = document.querySelector("div.container");
-for(i=0;i<names.length;i++)
-{
-  mydiv= document.createElement('DIV');
-  if(i===0)
-  mydiv.className=('grid-item-leader');
-  else
-  mydiv.className=('grid-item');
+for (i = 0; i < names.length; i++) {
+  mydiv = document.createElement("DIV");
+  if (i === 0) mydiv.className = "grid-item-leader";
+  else mydiv.className = "grid-item";
   mydiv.innerHTML = names[i];
   odiv.appendChild(mydiv);
 }
