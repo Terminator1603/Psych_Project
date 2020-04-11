3539dab9fc84832b1c855398b83853cd2a923724
Yash Burshe <bursheyash@icloud.com>
2020-04-12 00:01:08 +0530

    switched to bem

--- lobbyScript.js
+++ lobbyScript.js
@@ -1,11 +1,11 @@
-var names = ["yash", "rohit", "akis"];
+var names = ["Yash", "Rohit", "Akis"];
 var i = 0;
 var mydiv;
-var odiv = document.querySelector("div.container");
+var odiv = document.querySelector("div.container--grid--lobby");
 for (i = 0; i < names.length; i++) {
   mydiv = document.createElement("DIV");
-  if (i === 0) mydiv.className = "grid-item-leader";
-  else mydiv.className = "grid-item";
+  if (i === 0) mydiv.className = "grid_item grid_item--leader";
+  else mydiv.className = "grid_item";
   mydiv.innerHTML = names[i];
   odiv.appendChild(mydiv);
 }
