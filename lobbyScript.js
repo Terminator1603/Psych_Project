4258114eb1e1fabe8f468fdc6e1b5b3895eb7242
Yash Burshe <bursheyash@icloud.com>
2020-04-12 15:25:14 +0530

    fixed bem

--- lobbyScript.js
+++ lobbyScript.js
@@ -1,7 +1,7 @@
 var names = ["Yash", "Rohit", "Akis"];
 var i = 0;
 var mydiv;
-var odiv = document.querySelector("div.container--grid--lobby");
+var odiv = document.querySelector("div.container--lobby");
 for (i = 0; i < names.length; i++) {
   mydiv = document.createElement("DIV");
   if (i === 0) mydiv.className = "grid_item grid_item--leader";
